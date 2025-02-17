2ffdfdcf7b0e34be4c58a07845933dca4a87877d
kenrique100 <ngwakenri2016@gmail.com>
2025-02-17 10:37:15 -0500

    duplicated code entries

--- src/main/java/com.akentech.microservices.inventory/util/InventoryUtil.java
+++
@@ -1,8 +0,0 @@
-package com.akentech.microservices.inventory.util;
-
-public class InventoryUtil {
-
-    public static boolean isQuantityValid(Integer quantity) {
-        return quantity != null && quantity >= 0;
-    }
-}
